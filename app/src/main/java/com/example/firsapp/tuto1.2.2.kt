31c4c01ff9c2746bda7f3b90277a7991af389adf
CHABBEH-Aymen <chabbehaymensolicode@gmail.com>
2024-10-08 13:07:03 +0100

    first commit

---
+++ app/src/main/java/com/example/firsapp/tuto1.2.2.kt
@@ -0,0 +1,5 @@
+package com.example.firsapp
+
+fun main() {
+    println("Hello World");
+}
---
+++ app/src/main/java/com/example/firsapp/tutoFunctions.kt
@@ -0,0 +1,10 @@
+package com.example.firsapp
+
+fun main() {
+    name(name  = "Aymen");
+}
+
+fun name(name: String): String
+{
+    return "Hello $name";
+}
