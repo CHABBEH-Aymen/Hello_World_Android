5f5f01939896209e7128cab81108a7c922a6ed4d
CHABBEH-Aymen <chabbehaymensolicode@gmail.com>
2024-10-09 11:47:05 +0100

    added tuto 1.4

---
+++ app/src/main/java/com/example/firsapp/tuto1.4.kt
@@ -0,0 +1,9 @@
+package com.example.firsapp
+
+fun main() {
+    fun birthdayGreeting(name: String = "Rover", age: Int): String {
+        return "Happy Birthday, $name! You are now $age years old!"
+    }
+    println(birthdayGreeting(age = 5))
+    println(birthdayGreeting("Rex", 2))
+}
